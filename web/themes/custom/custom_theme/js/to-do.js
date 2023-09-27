44f82df9b8706e9ba52f5f27f3234ab9840ec4f4
punit <punit.pradhan@innoraft.com>
2023-09-27 22:45:20 +0530

    FT2023-417: optimized my code

--- web/themes/custom/custom_theme/js/to-do.js
+++ web/themes/custom/custom_theme/js/to-do.js
@@ -1,78 +1,56 @@
-/**
- * @file
- * Provides the functionality to add or remove todo list items.
- */
-
 (function ($, Drupal, once) {
-    Drupal.behaviors.customTodoBehavior = {
-      attach: function (context, settings) {
-        // Apply the customTodo effect to the elements only once.
-        const $inputField = $('#custom-to-do-list');
-        const $savedItemList = $('#custom-saved-items');
-        const $emptyMessage = $('#custom-empty-message');
-        const customTodoListName = 'custom-todo-list';
-        let customTodoList = JSON.parse(localStorage.getItem(customTodoListName)) || [];
-  
-        once('customTodo', $inputField, context).forEach(function (element) {
-          $('#custom-submit-button').on('click', function (event) {
-            event.preventDefault();
-            const $newItem = $inputField.val().trim();
-  
-            if ($newItem !== '') {
-              $emptyMessage.hide();
-              $savedItemList.show();
-              $savedItemList.append(`<li>${$newItem}</li>`);
-              saveCustomToDoItem($newItem);
-              clearInputField();
-            }
-          });
-  
-          $('#custom-clear-button').on('click', function (event) {
-            event.preventDefault();
-            clearAllCustomToDoItems();
-          });
-  
-          function saveCustomToDoItem($item) {
-            const itemList = getCustomToDoList();
-            itemList.push($item);
-            localStorage.setItem(customTodoListName, JSON.stringify(itemList));
-          }
-  
-          function getCustomToDoList() {
-            return JSON.parse(localStorage.getItem(customTodoListName)) || [];
-          }
-  
-          function clearInputField() {
-            $inputField.val('');
-          }
-  
-          function clearDisplay() {
-            $savedItemList.hide();
-            $emptyMessage.show();
-            clearInputField();
-          }
-  
-          function displayCustomToDoList() {
-            const itemList = getCustomToDoList();
-            if (itemList.length > 0) {
-              itemList.forEach(function (item) {
-                $savedItemList.append(`<li>${item}</li>`);
-              });
-            } else {
-              clearDisplay();
-            }
-          }
-  
-          displayCustomToDoList();
-  
-          function clearAllCustomToDoItems() {
-            localStorage.removeItem(customTodoListName);
-            customTodoList = [];
-            $savedItemList.empty();
-            clearDisplay();
-          }
-        });
+  Drupal.behaviors.todoBehavior = {
+    attach: function (context, settings) {
+      const todoListName = 'custom_todo_list';
+      const $listElement = $('#custom-to-do-list');
+      const $savedItemUl = $('#custom-saved-item-ul');
+      const $emptyList = $('#custom-to-do-list-empty');
+      let todoList = JSON.parse(localStorage.getItem(todoListName)) || [];
+
+      function addTodoItem() {
+        const $todoInput = $listElement.val().trim();
+
+        if ($todoInput !== '') {
+          $emptyList.hide();
+          $savedItemUl.show();
+          $savedItemUl.append(`<li>${$todoInput}</li>`);
+          saveTodoItem($todoInput);
+          $listElement.val('');
+        }
       }
-    };
-  })(jQuery, Drupal, once);
-  
+
+      function clearTodoList() {
+        deleteTodoList();
+        $savedItemUl.hide();
+        $emptyList.show();
+      }
+
+      function saveTodoItem($inputItem) {
+        todoList.push($inputItem);
+        localStorage.setItem(todoListName, JSON.stringify(todoList));
+      }
+
+      function showTodoList(element) {
+        $savedItemUl.append(`<li>${element}</li>`);
+      }
+
+      function deleteTodoList() {
+        localStorage.removeItem(todoListName);
+        todoList = [];
+        $savedItemUl.empty();
+      }
+
+      once('customTodo', $listElement, context).forEach(function (element) {
+        $('#custom-to-do-submit').on('click', addTodoItem);
+        $('#custom-to-do-clear').on('click', clearTodoList);
+
+        if (todoList.length > 0) {
+          todoList.forEach(showTodoList);
+        } else {
+          $emptyList.show();
+          $savedItemUl.hide();
+        }
+      });
+    },
+  };
+})(jQuery, Drupal, once);
